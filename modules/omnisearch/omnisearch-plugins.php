d63bdece838b6861cb7a8f8e62efe75342d92bcb
George Stephanis <george@stephanis.info>
2013-05-19 22:58:59 -0400

    Converting the plugin search to an AJAX request to improve page load time.

--- modules/omnisearch/omnisearch-plugins.php
+++ modules/omnisearch/omnisearch-plugins.php
@@ -6,13 +6,20 @@ class Jetpack_Omnisearch_Plugins {
 	function __construct() {
 		self::$instance = $this;
 		add_filter( 'omnisearch_results', array( $this, 'search'), 10, 2 );
+		add_action( 'wp_ajax_omnisearch_plugins', array( $this, 'wp_ajax_omnisearch_plugins' ) );
 	}
 
 	function search( $results, $search_term ) {
 		$search_link = ' <a href="' . admin_url( "plugin-install.php?tab=search&s={$search_term}" ) . '" class="add-new-h2">' . __('Search Plugins') . '</a>';
 		$html = '<h2>' . __('Plugins') . $search_link . '</h2>';
 
-		$html .= $this->results_html( $search_term );
+	#	$html .= $this->results_html( $search_term );
+
+		$html .= '<div id="' . __CLASS__ . '_results">' . __('Loading &hellip;') . '</div>';
+
+		$html .= '<script>jQuery(document).ready(function($){'
+					.'$("#' . __CLASS__ . '_results").load(ajaxurl,{action:"omnisearch_plugins",search_term:search_term});'
+				.'});</script>';
 
 		$results[ __CLASS__ ] = $html;
 		return $results;
@@ -41,6 +48,11 @@ class Jetpack_Omnisearch_Plugins {
 		}
 
 		return $html;
+	}
+
+	function wp_ajax_omnisearch_plugins() {
+		$search_term = $_REQUEST['search_term'];
+		wp_die( $this->results_html( $search_term ) );
 	}
 
 }
